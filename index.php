811151489674420f518a7d2a4526962a30f8e9b2
Michael Neath <49969734+mneath@users.noreply.github.com>
2020-05-02 15:13:41 +1000

    commit
    this is

--- index.php
+++ index.php
@@ -4,5 +4,19 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
+ 
+ 
+ <?php
+// the message
+$msg = "First line of text\nSecond line of text";
+
+// use wordwrap() if lines are longer than 70 characters
+$msg = wordwrap($msg,70);
+
+// send email
+mail("neathmj@gmail.com","My subject",$msg);
+?>
+
+
  </body>
 </html>
